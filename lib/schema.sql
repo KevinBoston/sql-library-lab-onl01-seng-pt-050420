11e1cf61fc19b61fc949577e32e122f88da126f3
Kevin Boston <kbos22@gmail.com>
2020-07-01 15:54:54 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -4,20 +4,24 @@ title TEXT,
 author_id INTEGER,
 subgenre_id INTEGER
 );
+
 CREATE TABLE subgenres (
 id INTEGER PRIMARY KEY, 
 name TEXT
 );
+
 CREATE TABLE authors (
 id INTEGER PRIMARY KEY, 
 name TEXT
 );
+
 CREATE TABLE books (
 id INTEGER PRIMARY KEY, 
 title TEXT,
 year INTEGER,
 series_id INTEGER 
 );
+
 CREATE TABLE characters (
 id INTEGER PRIMARY KEY, 
 name TEXT,
@@ -25,6 +29,7 @@ motto TEXT,
 species TEXT,
 author_id INTEGER
 );
+
 
 CREATE TABLE character_books (
 id INTEGER PRIMARY KEY, 
