bb060cd123b438b5e2162c202030dd838800431c
Kevin Boston <kbos22@gmail.com>
2020-07-01 16:33:08 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -16,7 +16,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title FROM series JOIN 
+  "SELECT series.title FROM series JOIN books ON series.
   
   WHERE characters.species = 'human'
     GROUP BY series.title
